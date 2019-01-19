d5cdc6c11a1d4d7cf619ab7385f3503f268971eb
Jim Doyle <indellibleink@gmail.com>
2019-01-19 12:19:15 -0500

    got the number in the array tovsdd up

--- comparingStrings.js
+++ comparingStrings.js
@@ -1,11 +1,13 @@
 var compare = ["bear", "this", 4];
 var output = [];
+var total = 0;
 for (var i = 0; i < compare[2]; i++) {
   word1 = compare[0].charCodeAt(i);
   word2 = compare[1].charCodeAt(i);
   word3 = word2 - word1;
   output.push(word3);
+  var total = total + output[i];
 }
-console.log(output.join(","));
-
+process.stdout.write(output.join(","));
+process.stdout.write(" = " + total + "\n");
 // TODO: add numbers
--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-
-        var dividerDown =
-          divider % 2 !== 0
-            ? process.stdout.write(board.dash)
-            : process.stdout.write(board.plus);
-        process.stdout.write("\n");
-      } 
