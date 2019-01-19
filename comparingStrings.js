43e352e92bf15e20d030852b3aa75fb2e092a51b
Jim Doyle <indellibleink@gmail.com>
2019-01-18 23:45:42 -0500

    refactored: changed: out to console to out into array to console

--- comparingStrings.js
+++ comparingStrings.js
@@ -1,8 +1,9 @@
 var a = ["bear", "this", 4];
+var output = [];
 for (var i = 0; i < a[2]; i++) {
   word1 = a[0].charCodeAt(i);
   word2 = a[1].charCodeAt(i);
   word3 = word2 - word1;
-  process.stdout.write(word3 + " ");
+  output.push(word3);
 }
-process.stdout.write("\nWord length: " + a[2] + "\n");
+console.log(output.join(","));
