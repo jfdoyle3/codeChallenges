9a874949582114419fe8025c9ff7eedb78945483
Jim Doyle <indellibleink@gmail.com>
2019-01-20 01:14:42 -0500

    Working on data collection

--- ticTacToe/drawBoard1.js
+++ ticTacToe/drawBoard1.js
@@ -1,10 +1,15 @@
 drawBoard(3, 3);
 play(1, 2, "X");
 function play(x, y, mark) {
-  var vBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-  vBoard[x][y] = mark;
+  //var vBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+  //vBoard[x][y] = mark;
   //  vBoard[0][0] = "X";
-  console.log(vBoard);
+  var vBoard=[1,2,3,4,5,6,7,8,9];
+ for (var i=1; i<=vBoard.length; i++){
+   console.log(i);
+ }
+
+  
 }
 function drawBoard(across, down) {
   var board = {
@@ -19,17 +24,13 @@ function drawBoard(across, down) {
   for (var grid = 1; grid <= board.down; grid++) {
     for (var space = 1; space <= board.across + 2; space++) {
       var spaceAcross =
-        space % 2 !== 0
-          ? process.stdout.write(board.space)
-          : process.stdout.write(board.line);
+        space % 2 !== 0 ? process.stdout.write(board.space) : process.stdout.write(board.line);
     }
     process.stdout.write("\n");
     if (grid % 2 == 0 || grid == 1) {
       for (var divider = 1; divider <= board.down + 2; divider++) {
         var dividerDown =
-          divider % 2 !== 0
-            ? process.stdout.write(board.dash)
-            : process.stdout.write(board.plus);
+          divider % 2 !== 0 ? process.stdout.write(board.dash) : process.stdout.write(board.plus);
       }
       process.stdout.write("\n");
     }
