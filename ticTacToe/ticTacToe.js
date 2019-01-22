09f9427d4e464555f711eceba995e44b13353019
Jim Doyle <indellibleink@gmail.com>
2019-01-21 21:01:40 -0500

    function: a play on the virtual board can be made.

--- ticTacToe/ticTacToe.js
+++ ticTacToe/ticTacToe.js
@@ -1,8 +1,8 @@
 drawBoard(3, 3);
-play(1, 2, "X");
-function play(x, y, mark) {
-  var vBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-  vBoard[x][y] = mark;
+play(5, "X");
+function play(move, token) {
+  var vBoard = [1, 2, 3,4, 5, 6, 7, 8, 9];
+  vBoard[--move] =token;
   //  vBoard[0][0] = "X";
   console.log(vBoard);
 }
@@ -18,18 +18,12 @@ function drawBoard(across, down) {
 
   for (var grid = 1; grid <= board.down; grid++) {
     for (var space = 1; space <= board.across + 2; space++) {
-      var spaceAcross =
-        space % 2 !== 0
-          ? process.stdout.write(board.space)
-          : process.stdout.write(board.line);
+      var spaceAcross = space % 2 !== 0 ? process.stdout.write(board.space) : process.stdout.write(board.line);
     }
     process.stdout.write("\n");
     if (grid % 2 == 0 || grid == 1) {
       for (var divider = 1; divider <= board.down + 2; divider++) {
-        var dividerDown =
-          divider % 2 !== 0
-            ? process.stdout.write(board.dash)
-            : process.stdout.write(board.plus);
+        var dividerDown = divider % 2 !== 0 ? process.stdout.write(board.dash) : process.stdout.write(board.plus);
       }
       process.stdout.write("\n");
     }
