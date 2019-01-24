eea676556164ae8d34f7dad4bec8266c5ed2392d
Jim Doyle <indellibleink@gmail.com>
2019-01-24 05:57:56 -0500

    Update ticTacToe.js

--- ticTacToe/ticTacToe.js
+++ ticTacToe/ticTacToe.js
@@ -1,11 +1,11 @@
 const { drawBoard } = require("./drawBoard");
-const { move } = require("./move");
-const { isValid } = require("./isValid");
+// const { move } = require("./move");
+// const { isValid } = require("./isValid");
 
 const vBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 vBoard[9] = "X";
 marked = vBoard[9];
-console.log(move(9, "O"));
+// console.log(move(9, "O"));
 
 console.log(drawBoard(3, 3));
-isValid(marked);
+// isValid(marked);
