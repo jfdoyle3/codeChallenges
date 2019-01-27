2fba877f8ddeac9bde492c96e869f727fc4cee0d
Jim Doyle <indellibleink@gmail.com>
2019-01-27 09:28:26 -0500

    Working on win module: math on winning combos

--- ticTacToe/modules/win.js
+++ ticTacToe/modules/win.js
@@ -1,23 +1,47 @@
-var a, b, c;
-const z = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]; // X wins
-//const z = ["O","O","O","O","O","O","O","O","O"];  // O wins
+var a, b, c, i;
+const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+//const z = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]; // X wins
+//const z = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]; // O wins
 //const z = ["X", "O", "O", "O", "X", "X", "X", "O", "O"]; //Draw
+const z = ["O", "X", "O", "X", "X", "X", "X", "O", "O"]; // X wins
+//const z = ["X", "O", "O", "O", "X", "X", "X", "O", "O"]; // O wins
 //const z=[1,2,3,4,5,6,7,8,9];
 //const z=[[1,2,3],[4,5,6],[7,8,9]];
 //const z=[["X,"X","X"],["X","X","X"],["X","X","X"]];
 
 //const x = ["X", "O"];
+for (var i = 0; i < z.length; i++) {
+  if (i % 3 == 0) {
+    process.stdout.write("\n");
+  }
+  process.stdout.write(grid[i] + "[" + z[i] + "]");
+}
+console.log("\n");
 
-//a = z[0];
-//b = z[1];
-//c = z[2];
-console.log(z.indexOf("G"));
-//win(a, b, c);
+i = 0;
+while (i < z.length) {
+  //  if (z[i] == "X" && z[i] == "X" && z[i] == "X") {
+  //console.log("Wins!!! ");
+  //    break;
+  //  }
+  i++;
+}
 
-//function win(a, b, c) {
-//  if (a == "X" && b == "X" && c == "X") {
-//    console.log("Wins!!!");
-//  } else {
-//    console.log("lost");
-//  }
-//}
+//  Notes:
+// math works as a 1 base index.
+// space/int , X  , O
+// a,b,c -check for same character
+// across  +1
+// down  +3
+// diag:\:  +4
+// diag:/:  +2
+//
+// Win totals
+//  123  =  6
+//  456  =  15
+//  789  =  24
+//  = = =
+//  12 15 18
+//
+//  159  =  15
+//  357  =  15
