5e82ef06f104e5477ff4b092710f433a5e669aed
Jim Doyle <indellibleink@gmail.com>
2019-01-29 23:18:59 -0500

    Got Winning to work

--- ticTacToe/modules/win.js
+++ ticTacToe/modules/win.js
@@ -1,15 +1,11 @@
-//TODO: refactor!!
-const win = ()=>{
+
 const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 //const z = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]; // X wins
 //const z = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]; // O wins
 //const z = ["X", "O", "O", "O", "X", "X", "X", "O", "O"]; //Draw
 //const z = ["O", "X", "O", "X", "X", "X", "X", "O", "O"]; // X wins
 const z = ["X", "O", "O", "O", "X", "X", "O", "O", "O"]; // O wins
-//const z=["X","X","X","O"," ","O"];
-
-
-//var token = ["X", "O"];
+//const z=["X","X","X","O"," ","O"]; // X wins easily - unfinished game
 for (var i = 0; i < z.length; i++) {
  if (i % 3 == 0) {
    process.stdout.write("\n");
@@ -22,7 +18,7 @@ var x = [];
 var o=[];
 
 for (var w = 0; w < z.length; w++) {
-  token = z[w];
+ var token = z[w];
   if (token == "X") {
     x.push(w);
   } else
@@ -30,23 +26,21 @@ for (var w = 0; w < z.length; w++) {
     o.push(w);
   }
 }
-console.log (o+"  "+x);
 var winX = x.join("");
 var winO = o.join("");
-//console.log (winX,winO);
-
-const conditions=[/012/g,/345/g,/678/g,/036/g,/147/g,/258/g,/047/g,/246/g];
-for (var p=0; p<conditions.length; p++)
-if (winX.match(conditions[p])) {
-  console.log("X Wins");
-  break;
-} else if (winO.match(conditions[p])){
+console.log(winX+"   "+winO);
+const everyWin=[/012/g,/345/g,/678/g,/036/g,/147/g,/258/g,/047/g,/246/g];
+console.log(everyWin[0]);
+for (var s=0; s<e.length; s++)
+if (winX.match(everyWin[spaces])) {
+      console.log("X Wins");
+      break;
+    } else if (winO.match(everyWin[spaces])){
   console.log("O Wins");
   break;
-} else {
+    } else {
   console.log("Draw");
-    break;
+   break;
   }
-}
-module.exports.win = win;
+
 
