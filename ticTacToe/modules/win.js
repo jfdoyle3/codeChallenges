76e30d4446f23a45b96fa714b4bc34fbdc214b0c
Jim Doyle <indellibleink@gmail.com>
2019-01-30 09:50:55 -0500

    renaming varibles from simple to verbose

--- ticTacToe/modules/win.js
+++ ticTacToe/modules/win.js
@@ -1,4 +1,3 @@
-
 const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 //const z = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]; // X wins
 //const z = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]; // O wins
@@ -7,40 +6,43 @@ const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const z = ["X", "O", "O", "O", "X", "X", "O", "O", "O"]; // O wins
 //const z=["X","X","X","O"," ","O"]; // X wins easily - unfinished game
 for (var i = 0; i < z.length; i++) {
- if (i % 3 == 0) {
-   process.stdout.write("\n");
- }
- process.stdout.write(grid[i] + "[" + z[i] + "]");
+  if (i % 3 == 0) {
+    process.stdout.write("\n");
+  }
+  process.stdout.write(grid[i] + "[" + z[i] + "]");
 }
 console.log("\n");
 
 var x = [];
-var o=[];
+var o = [];
 
 for (var w = 0; w < z.length; w++) {
- var token = z[w];
+  var token = z[w];
   if (token == "X") {
     x.push(w);
-  } else
-  if(token=="O"){
+  } else if (token == "O") {
     o.push(w);
   }
 }
 var winX = x.join("");
 var winO = o.join("");
-console.log(winX+"   "+winO);
-const everyWin=[/012/g,/345/g,/678/g,/036/g,/147/g,/258/g,/047/g,/246/g];
-console.log(everyWin[0]);
-for (var s=0; s<e.length; s++)
-if (winX.match(everyWin[spaces])) {
-      console.log("X Wins");
-      break;
-    } else if (winO.match(everyWin[spaces])){
-  console.log("O Wins");
-  break;
-    } else {
-  console.log("Draw");
-   break;
+console.log(winX + "   " + winO);
+const everyWin = [
+  /012/g,
+  /345/g,
+  /678/g,
+  /036/g,
+  /147/g,
+  /258/g,
+  /047/g,
+  /246/g
+];
+//console.log(everyWin[0]);
+for (var s = 0; s < everyWin.length; s++)
+  if (winX.match(everyWin[s])) {
+    console.log("X Wins");
+  } else if (winO.match(everyWin[s])) {
+    console.log("O Wins");
+  } else {
+    console.log("Draw");
   }
-
-
