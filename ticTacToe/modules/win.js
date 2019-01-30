4e75666560f41637a3b7e2ecdb07ff7b0c2c1ce5
Jim Doyle <indellibleink@gmail.com>
2019-01-30 18:43:19 -0500

    need to filter out win

--- ticTacToe/modules/win.js
+++ ticTacToe/modules/win.js
@@ -1,10 +1,12 @@
 const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-//const z = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]; // X wins
+const z = ["X", "O", "X", "X", "O", "O", "X", " ", " "]; // X wins
 //const z = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]; // O wins
 //const z = ["X", "O", "O", "O", "X", "X", "X", "O", "O"]; //Draw
 //const z = ["O", "X", "O", "X", "X", "X", "X", "O", "O"]; // X wins
 //const z = ["X", "O", "O", "O", "X", "X", "O", "O", "O"]; // O wins
-const z = ["X", "X", "X", "O", " ", "O"]; // X wins easily - unfinished game
+//const z = ["X", "O", "O", "O", "X", "X", "O", "O", "X"]; // X wins diag 0,4,8
+//const z = ["X", "O", "O", "O", "O", "X", "O", "O", "O"]; // O wins diag 2,4,6
+
 for (var i = 0; i < z.length; i++) {
   if (i % 3 == 0) {
     process.stdout.write("\n");
@@ -27,23 +29,16 @@ for (var w = 0; w < z.length; w++) {
 var winX = x.join("");
 var winO = o.join("");
 console.log("X: " + winX + " | O: " + winO);
-const allWins = [
-  /012/g,
-  /345/g,
-  /678/g,
-  /036/g,
-  /147/g,
-  /258/g,
-  /047/g,
-  /246/g
-];
+// Process string remove useless moves /sort win
+
+//const allWins = [/012/g, /345/g, /678/g, /036/g, /147/g, /258/g, /04/g, /246/g];
 
 for (var result = 0; result < allWins.length; result++) {
   if (winX.match(allWins[result])) {
-    console.log("X Wins");
+    console.log("found: " + allWins[result] + " X Wins");
     break;
   } else if (winO.match(allWins[result])) {
-    console.log("O Wins");
+    console.log("found: " + allWins[result] + " O Wins");
     break;
   }
 }
