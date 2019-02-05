2590ea151de073d614d6ce1cff7c6defe25df65b
Jim Doyle <indellibleink@gmail.com>
2019-02-05 03:14:33 -0500

    Working and getting results of using map

--- ticTacToe/Map_Reduce_Filter_sandbox.js
+++ ticTacToe/Map_Reduce_Filter_sandbox.js
@@ -5,9 +5,10 @@ const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 //const z = ["X", "O", "O", "O", "X", "X", "X", "O", "O"]; //Draw
 //const z = ["O", "X", "O", "X", "X", "X", "X", "O", "O"]; // X wins
 //const z = ["X", "O", "X", "O", "X", "X", "O", "O", "O"]; // O wins
-const z = ["X", "O", "O", "O", "X", "X", "O", "O", "X"]; // X wins diag 0,4,8
+//const z = ["X", "O", "O", "O", "X", "X", "O", "O", "X"]; // X wins diag 0,4,8
+//const z = ["O", "X", "O", "X", "O", "O", "X", "X", "O"]; // O wins diag 0,4,8
 //const z = ["X", "O", "O", "O", "O", "X", "O", "O", "O"]; // O wins diag 2,4,6
-//const z = [" ", " ", " ", " ", "O", "X", "O", "O", "O"]; // X Win
+const z = [" ", " ", " ", " ", "O", "X", "O", "O", "O"]; // X Win
 //
 for (var i = 0; i < z.length; i++) {
   if (i % 3 == 0) {
@@ -16,12 +17,15 @@ for (var i = 0; i < z.length; i++) {
   process.stdout.write(grid[i] + "[" + z[i] + "]");
 }
 console.log("\n");
-var places = z.map(token => {
+var x = z.map(token => {
   return token === "X";
 });
-console.log(places);
-if (places[0] && places[4] && places[8]) {
-  console.log("Win");
+
+console.log(x);
+if (x[6] && x[7] && x[8]) {
+  console.log("X Win");
+} else {
+  console.log ("ELSE: X to win 048: O Wins");
 }
 
 //for (var i = 0; i < xPlace.length; i++) {
---
+++ ticTacToe/modules/gameover.js
@@ -0,0 +1,6 @@
+  const z = ["X", " ", "O", "O", "X", "X", "X", "O", "O"]; //Draw
+
+  const gameOver= z.map(token => {
+     if (token === " ")
+     console.log('game goes on');
+  });
