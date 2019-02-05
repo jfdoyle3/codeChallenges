82cf68d5f352642e0db3241997567a0fb782a869
Jim Doyle <indellibleink@gmail.com>
2019-02-04 22:44:58 -0500

    Got .map to worked

--- ticTacToe/Map_Reduce_Filter_sandbox.js
+++ ticTacToe/Map_Reduce_Filter_sandbox.js
@@ -1,10 +1,10 @@
 //const drawBoard = () => {
 const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-const z = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]; // X wins
+//const z = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]; // X wins
 //const z = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]; // O wins
 //const z = ["X", "O", "O", "O", "X", "X", "X", "O", "O"]; //Draw
 //const z = ["O", "X", "O", "X", "X", "X", "X", "O", "O"]; // X wins
-//const z = ["X", "O", "O", "O", "X", "X", "O", "O", "O"]; // O wins
+const z = ["X", "O", "O", "O", "X", "X", "O", "O", "O"]; // O wins
 //const z = ["X", "O", "O", "O", "X", "X", "O", "O", "X"]; // X wins diag 0,4,8
 //const z = ["X", "O", "O", "O", "O", "X", "O", "O", "O"]; // O wins diag 2,4,6
 //const z = [" ", " ", " ", " ", "O", "X", "O", "O", "O"]; // X Win
@@ -15,12 +15,14 @@ for (var i = 0; i < z.length; i++) {
   }
   process.stdout.write(grid[i] + "[" + z[i] + "]");
 }
-var Xs = function(x) {
+var xPlace = z.map(x => {
   return x === "X";
-};
-var xplace = z.filter(Xs);
-console.log(xplace);
-// console.log("\n");
+});
+console.log(xPlace);
+//for (var i = 0; i < xPlace.length; i++) {
+console.log(xPlace[0]);
+//}
+// console.log("\n");\\
 // console.log(xLoCal);
 //};
 //module.exports.wins = wins;
