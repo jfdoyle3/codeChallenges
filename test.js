7979053ce85bf937d3f40065b2fd4b95f6c03f15
Jim Doyle <indellibleink@gmail.com>
2019-02-04 19:28:28 -0500

    got filter to work

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 var z = [1, 2, 4, 3];
+var x = [5, 5, 5];
 var found = [];
+
 for (var i = 0; i < z.length; i++) {
-  if (z[i] == 1 || z[i] == 2 || z[i] == 3) {
-    found.push(z[i]);
-    console.log(found + "    all numbers");
-  } else {
-    console.log("no numbers");
-  }
+  found.push(z[i]);
 }
+console.log(all);
--- ticTacToe/Map_Reduce_Filter_sandbox.js
+++ ticTacToe/Map_Reduce_Filter_sandbox.js
@@ -1,3 +1,26 @@
-var name = ["Theo", "Rose", "Ed", "Jim"];
-var x = name.map(index => index.toUpperCase());
-console.log(x);
+//const drawBoard = () => {
+const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const z = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]; // X wins
+//const z = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]; // O wins
+//const z = ["X", "O", "O", "O", "X", "X", "X", "O", "O"]; //Draw
+//const z = ["O", "X", "O", "X", "X", "X", "X", "O", "O"]; // X wins
+//const z = ["X", "O", "O", "O", "X", "X", "O", "O", "O"]; // O wins
+//const z = ["X", "O", "O", "O", "X", "X", "O", "O", "X"]; // X wins diag 0,4,8
+//const z = ["X", "O", "O", "O", "O", "X", "O", "O", "O"]; // O wins diag 2,4,6
+//const z = [" ", " ", " ", " ", "O", "X", "O", "O", "O"]; // X Win
+//
+for (var i = 0; i < z.length; i++) {
+  if (i % 3 == 0) {
+    process.stdout.write("\n");
+  }
+  process.stdout.write(grid[i] + "[" + z[i] + "]");
+}
+var Xs = function(x) {
+  return x === "X";
+};
+var xplace = z.filter(Xs);
+console.log(xplace);
+// console.log("\n");
+// console.log(xLoCal);
+//};
+//module.exports.wins = wins;
