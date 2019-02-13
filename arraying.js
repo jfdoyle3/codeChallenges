c10282e085c856abeb4eecf6894f2e7b9feeb388
Jim Doyle <indellibleink@gmail.com>
2019-02-13 08:58:45 -0500

    working on arrays

--- arraying.js
+++ arraying.js
@@ -2,6 +2,8 @@ const a = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 const b = [258];
 const c = [1, 2, 3, 4];
 const d = ["_", "_", "X", "_", "_", "X", "_", "_", "X"];
+
+// Draw Board
 for (var i = 0; i < a.length; i++) {
   if (i % 3 == 0) {
     process.stdout.write("\n");
@@ -9,10 +11,14 @@ for (var i = 0; i < a.length; i++) {
   process.stdout.write(a[i]);
 }
 process.stdout.write("\n");
-var z = d.filter(x => (x = "X"));
-//for (let e of z) {
-//  console.log(e);
-//}
 
-d.forEach();
-console.log(z);
+//Determine Win
+
+let moves = 9;
+
+while (i < moves) {
+  let find = "X";
+  if (d[i] == find) {
+  }
+  i++;
+}
